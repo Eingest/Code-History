3ceb8397c0e404fe3531c09c694bcff619602838
Eingest <ak0805@live.de>
2022-03-16 16:52:25 +0100

    a lot of stuff

---
+++ Code History/Models/Game.swift
@@ -0,0 +1,12 @@
+//
+//  Game.swift
+//  Code History
+//
+//  Created by Andreas Kiesel on 16.03.22.
+//
+
+import Foundation
+
+struct Game {
+    
+}
---
+++ Code History/Models/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Code History
+//
+//  Created by Andreas Kiesel on 16.03.22.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
